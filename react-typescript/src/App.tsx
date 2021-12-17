8c90ccfc522fee32b4af82e9acb158c7a2daa4fa
MostafaNazmulHasan <mostafanazmulhasan@gmail.com>
2021-12-17 22:49:50 +0600

    Typescript With React

---
+++ react-typescript/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Contacts from './components/Contacts';
+
+function App() {
+  return (
+    <div className='contactList'>
+
+      <Contacts></Contacts>
+    </div>
+  );
+}
+
+export default App;
